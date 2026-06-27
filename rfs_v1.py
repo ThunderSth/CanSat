52a272258a03e30ad03e9547b23139596e2299e5
ThunderSth <bjorn0arvidsson@gmail.com>
2026-06-27 16:58:52 +0200

    Correct units and specifications for directions

--- rfs_v1.py
+++ rfs_v1.py
@@ -37,5 +37,8 @@ while True:
         calibrate()
         started_holding = time.time()
     else:
-        print(f"Acceleration x: {imu.acceleration[0]+acceleration_calibration[0]} m/s, Acceleration y:{imu.acceleration[1]+acceleration_calibration[1]} m/s, Acceleration z: {imu.acceleration[2]+acceleration_calibration[2]} m/s")
+        print(f"Acceleration x: {imu.acceleration[0]+acceleration_calibration[0]} m/s^2, Acceleration y:{imu.acceleration[1]+acceleration_calibration[1]} m/s^2, Acceleration z: {imu.acceleration[2]+acceleration_calibration[2]} m/s^2")
         print(f"Gyro x: {imu.gyro[0]+gyro_calibration[0]} rad/s, Gyro y:{imu.gyro[1]+gyro_calibration[1]} rad/s, Gyro z: {imu.gyro[2]+gyro_calibration[2]} rad/s \n")
+
+#X = Left/Right Y=Forward/Back Z = Up/Down
+#Gyro 1 = Pitch 2 = Roll 3 = Yaw
