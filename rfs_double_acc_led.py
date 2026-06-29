a926c024dfa0b23bb0bb35a9284fce74590a50d8
ThunderSth <bjorn0arvidsson@gmail.com>
2026-06-29 11:43:47 +0200

    finished actually trust

--- rfs_double_acc_led.py
+++ rfs_double_acc_led.py
@@ -5,15 +5,18 @@ import busio
 import digitalio
 import adafruit_bmp280
 import pwmio
+import random
 
 #RGB Light
 red = pwmio.PWMOut(board.D11)
 green = pwmio.PWMOut(board.D12)
 blue = pwmio.PWMOut(board.D13)
 
-elapsed_time = 0
+switch2 = digitalio.DigitalInOut(board.D25)
 
-time_delay = 0.1
+switch2.direction = digitalio.Direction.INPUT
+switch2.pull = digitalio.Pull.DOWN
+
 
 RED         = (65535,     0,     0)
 ORANGE      = (65535, 32768,     0)
@@ -33,10 +36,13 @@ def set_color(color):
     green.duty_cycle = color[1]
     blue.duty_cycle = color[2]
 
+file = open(f"/{random.randint(10000,99999)}.csv", "a")
+
 def write_data(message):
-    with open("/data.csv", "a") as file:
+    if switch2.value == False:        
         file.write(message)
         file.write("\n")
+        file.flush()
 
 write_data("time,acc_x_1,acc_y_1,acc_z_1_1,acc_x_2,acc_y_2,acc_z_1_2,gyro_1_1,gyro_2_1,gyro_3_1,gyro_1_2,gyro_2_2,gyro_3_2,altitude,temperature")
 
@@ -47,7 +53,7 @@ bmp = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
 accelerometer1 = adafruit_mpu6050.MPU6050(i2c)
 accelerometer2 = adafruit_mpu6050.MPU6050(i2c,address=0x68)
 set_color(BLUE)
-time.sleep(3)
+time.sleep(2)
 
 #Calibration button
 switch = digitalio.DigitalInOut(board.D10)
@@ -64,6 +70,7 @@ altitude_calibration = 0
 BUTTON_HOLD_TIME = 1
 started_holding = 0
 
+start_time = time.monotonic()
 
 def get_averaged_acceleration(position):
     return ((accelerometer1.acceleration[position]+acceleration_calibration_1[position])+(accelerometer2.acceleration[position]+acceleration_calibration_2[position]))/2
@@ -89,13 +96,11 @@ set_color(YELLOW)
 
 while True:
 
-    time.sleep(time_delay)
-    elapsed_time+=time_delay
     #Calibrate
     if not switch.value and time.time()-started_holding > BUTTON_HOLD_TIME:
         print("calibrating")
         set_color(PURPLE)
-        time.sleep(2)
+        time.sleep(1)
 
         acceleration_calibration_1 = []
         acceleration_calibration_2 = []
@@ -114,7 +119,8 @@ while True:
         #Barometer stuff
         print(f"Altitude: {bmp.altitude+altitude_calibration}")
         #Acceleration/gyro
-
+        
+        time.sleep(0.01)
 
         acc_x_1 = accelerometer1.acceleration[0]+acceleration_calibration_1[0]
         acc_y_1 = accelerometer1.acceleration[1]+acceleration_calibration_1[1]
@@ -133,9 +139,11 @@ while True:
         gyro_z_2 = accelerometer2.gyro[2]+gyro_calibration_2[2]
 
 
-        write_data(f"{elapsed_time},{acc_x_1},{acc_y_1},{acc_z_1},{acc_x_2},{acc_y_2},{acc_z_2},{gyro_x_1},{gyro_x_1},{gyro_z_1},{gyro_x_2},{gyro_x_2},{gyro_z_2},{bmp.altitude},{bmp.temperature}")
+        write_data(f"{time.monotonic()-start_time},{acc_x_1},{acc_y_1},{acc_z_1},{acc_x_2},{acc_y_2},{acc_z_2},{gyro_x_1},{gyro_x_1},{gyro_z_1},{gyro_x_2},{gyro_x_2},{gyro_z_2},{bmp.altitude},{bmp.temperature}")
         #print(f"Acceleration x: {get_averaged_acceleration(0)} m/s^2, Acceleration y:{get_averaged_acceleration(1)} m/s^2, Acceleration z: {get_averaged_acceleration(2)} m/s^2")
         #print(f"Gyro x: {get_averaged_gyro(0)} rad/s, Gyro y:{get_averaged_gyro(1)} rad/s, Gyro z: {get_averaged_gyro(2)} rad/s \n")
 
 #X = Left/Right Y=Forward/Back Z = Up/Down
 #Gyro 1 = Pitch 2 = Roll 3 = Yaw
+
+
